89f997821a5922aaa2d6f359d042cc97392fd34f
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-06-13 10:49:39 +0200

    feat: add Tailwind CSS and DaisyUI support
    - Updated package.json to include @tailwindcss/vite and daisyui dependencies.
    - Modified index.css to import Tailwind CSS and use DaisyUI plugin.
    - Updated vite.config.ts to include Tailwind CSS plugin in the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +13,7 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    react(),
+    react(), tailwindcss(),
     // ...,
   ],
 })
